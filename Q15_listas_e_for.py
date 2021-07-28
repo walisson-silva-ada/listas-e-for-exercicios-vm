5ae2bdfbb5afa1b93edfef048d04acd7d8c22aaa
Talita Fujimoto <talita.fujimoto@gmail.com>
2021-07-28 15:33:30 -0300

    mensagem que quiser

---
+++ Q15_listas_e_for.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+i = 0
+while i < n:
+    nota = float(input('Nota: '))
+    lista_nota.append(nota) 
+    i += 1
+
+media = sum(lista_nota)/len(lista_nota)
+media = round(media, 2)
+
+lista_media.append(media)
+
+media_TF = 0
+if media > 5:
+    media_TF = 'True'
+else:
+    media_TF = 'False'
+
+
+lista_media_TF.append(media_TF)
+
+count += 1
+
